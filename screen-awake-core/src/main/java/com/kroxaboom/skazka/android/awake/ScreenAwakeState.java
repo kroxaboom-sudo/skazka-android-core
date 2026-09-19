be773755da943cc0af5287da2279832995e6cf93
kroxaboom-sudo <kroxaboom@gmail.com>
2026-09-19 03:23:45 +0300

    feat: extract screen-awake and telemetry cores

---
+++ screen-awake-core/src/main/java/com/kroxaboom/skazka/android/awake/ScreenAwakeState.java
@@ -0,0 +1,11 @@
+package com.kroxaboom.skazka.android.awake;
+
+public enum ScreenAwakeState {
+    NEEDS_WRITE_SETTINGS,
+    NEEDS_NOTIFICATIONS,
+    READY,
+    ON_INFINITE,
+    ON_TIMER,
+    ON_CHARGING,
+    RECOVERY_REQUIRED
+}
